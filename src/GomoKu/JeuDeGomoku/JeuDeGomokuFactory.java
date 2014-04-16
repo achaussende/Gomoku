d4db0a519844b5b9b6a8d6b67b14f66fef5fc445
Slashgear <antoine395.caron@laposte.net>
2014-04-16 09:22:19 +0200

    Ajout d'une factory pour monteCarlo

--- src/GomoKu/JeuDeGomoku/JeuDeGomokuFactory.java
+++ src/GomoKu/JeuDeGomoku/JeuDeGomokuFactory.java
@@ -11,6 +11,7 @@ import GomoKu.PlateauJeu2D.JeuDePlateau2D;
 import GomoKu.PlateauJeu2D.JeuDePlateauFactory;
 import GomoKu.PlateauJeu2D.JoueurAleatoire;
 import GomoKu.PlateauJeu2D.JoueurHumain;
+import GomoKu.PlateauJeu2D.JoueurMonteCarlo;
 import java.util.ArrayList;
 
 /**
@@ -61,6 +62,27 @@ public class JeuDeGomokuFactory implements JeuDePlateauFactory {
             jeu.setPlateau(new PlateauGomoku(9, 9));
             jeu.setJoueur(1, new JoueurAleatoire(1));
             jeu.setJoueur(2, new JoueurAleatoire(2));
+        } catch (ExceptionPlateauJeu2D ex) {
+            System.out.println(ex.getMess_err());
+        }
+        if (situation != null) {
+            for (Coup c : situation) {
+                jeu.getPlateau().jouer(c);
+            }
+        }            
+
+        jeu.joueurSuivant();
+        return jeu;
+    }
+    
+    @Override
+    public JeuDePlateau2D CreerPartieHumainVSMonteCarlo(ArrayList<Coup> situation) {
+        JeuDePlateau2D jeu = new JeuDeGomoku();
+        JeuDeGomokuFactory fact=new JeuDeGomokuFactory();
+        try {
+            jeu.setPlateau(new PlateauGomoku(9, 9));
+            jeu.setJoueur(1, new JoueurAleatoire(1));
+            jeu.setJoueur(2, new JoueurMonteCarlo(2,10000,fact));
         } catch (ExceptionPlateauJeu2D ex) {
             System.out.println(ex.getMess_err());
         }
